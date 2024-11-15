5ca7a4c694f9066305915d8a7757072dfe2fe0d9
이원찬 <twoone14@tukorea.ac.kr>
2024-11-15 17:04:41 +0900

    Security Config 에 필터 추가

--- src/main/java/com/example/security/SecurityFiles/SecurityConfig.java
+++ src/main/java/com/example/security/SecurityFiles/SecurityConfig.java
@@ -9,10 +9,16 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import lombok.RequiredArgsConstructor;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+  private final JwtAuthFilter jwtAuthFilter;
+
   @Bean
   SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     http
@@ -30,7 +36,12 @@ public class SecurityConfig {
             authorize -> authorize
                 // 모든 요청에 대해 허용
                 .requestMatchers("/**")
-                .permitAll());
+                .permitAll())
+
+        .addFilterBefore(
+            jwtAuthFilter, UsernamePasswordAuthenticationFilter.class //
+        );
+
     return http.build();
   }
 
