f0ccfe7cb29a41e5e8d0a167e1df643fa3491602
이원찬 <twoone14@tukorea.ac.kr>
2024-11-15 16:15:50 +0900

    모든 요청 허용해보기

--- src/main/java/com/example/security/SecurityFiles/SecurityConfig.java
+++ src/main/java/com/example/security/SecurityFiles/SecurityConfig.java
@@ -2,8 +2,10 @@ package com.example.security.SecurityFiles;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -11,6 +13,22 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
   @Bean
   SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    http
+        // csrf 설정을 비활성화
+        .csrf(Customizer.withDefaults())
+        // http 기본 인증 해제
+        .httpBasic(Customizer.withDefaults())
+        // form 기반 인증 해제
+        .formLogin(Customizer.withDefaults())
+        // 세션 생성 정책 설정 (STATELESS: 세션을 사용하지 않음)
+        .sessionManagement(
+            authorize -> authorize
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        .authorizeHttpRequests(
+            authorize -> authorize
+                // 모든 요청에 대해 허용
+                .requestMatchers("/**")
+                .permitAll());
     return http.build();
   }
 }
